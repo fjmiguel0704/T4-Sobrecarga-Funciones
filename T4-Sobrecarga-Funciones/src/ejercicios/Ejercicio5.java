dc9fa9458e459289273cd38b01003ca1bba4dd91
fjmiguel0704 <fernandoj.miguel@iesnervion.es>
2022-12-02 11:01:06 +0100

    T4-Sobrecarga-Funciones

--- T4-Sobrecarga-Funciones/src/ejercicios/Ejercicio5.java
+++ T4-Sobrecarga-Funciones/src/ejercicios/Ejercicio5.java
@@ -11,10 +11,16 @@ public class Ejercicio5 {
 		cantidad=read.nextInt();
 		System.out.println("Valor máximo a generar: ");
 		valorMax=read.nextInt();
+		System.out.println("Valor mínimo a generar: ");
+		valorMin=read.nextInt();
+		
+		read.close();
 		
 		numerosAleatorios(cantidad);
-		System.out.println();
+		System.out.println("Valor máximo: ");
 		numerosAleatorios(cantidad, valorMax);
+		System.out.println("Entre mínimo y máximo: ");
+		numerosAleatorios(cantidad, valorMax, valorMin);
 		
 	}
 	
@@ -28,6 +34,15 @@ public class Ejercicio5 {
 		double numero;
 		for(int i = 1; i<=cantidad; i++ ) {
 			numero=Math.random()*max;
+			numero+=0.5;
+			System.out.println((int)numero);
+		}
+	}
+	
+	static void numerosAleatorios (int cantidad, int max, int min) {
+		double numero;
+		for(int i = 1; i<=cantidad; i++ ) {
+			numero=Math.random()*(max-min)+min;
 			numero+=0.5;
 			System.out.println((int)numero);
 		}
